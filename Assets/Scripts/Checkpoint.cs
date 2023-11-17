c5a5f717012edd124609414e87a80f8d5aae9896
Ngo Thai An <an.ngothai@gameloft.com>
2023-11-17 11:29:01 +0700

    Adjust camera size

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class Checkpoint : MonoBehaviour
 {
     [SerializeField] private PlayerController m_playerController;
     [SerializeField] private Animator m_animator;
     [SerializeField] private Transform m_respawnPoint;
+    [SerializeField] private  Light2D light2D;
     private bool TurnedOn = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -24,5 +26,6 @@ public class Checkpoint : MonoBehaviour
         m_playerController.respawnPosition = m_respawnPoint.position;
         yield return new WaitForSeconds(0.5f);
         m_animator.SetBool("On", true);
+        light2D.enabled = true;
     }
 }
