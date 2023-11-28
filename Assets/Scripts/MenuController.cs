5047b7e30c75db37dafb25896f1f26034b70cb11
Ngo Thai An <an.ngothai@gameloft.com>
2023-11-28 09:20:49 +0700

    Move cutscene to menu

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -2,10 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.Playables;
 public class MenuController : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private PlayableDirector director;
+    [SerializeField] private GameObject canvas;
+    //[SerializeField] private Animator menuAnimator;
+    private void Start()
+    {
+        if (PlayerPrefs.GetInt("PlayCutscene", 0) == 0)
+        {
+            PlayerPrefs.SetInt("PlayCutscene", 1);
+            director.Play();
+        }
+        else
+        {
+            StartMenu();
+        }
+    }
+
+    public void StartMenu()
+    {
+        canvas.SetActive(true);
+        animator.Play("MenuTransition_Open");
+    }
+
+    public void PlayCutscene()
+    {
+        animator.Play("MenuTransition_Close");
+        director.Play();
+        DisableCanvas(0.5f);
+    }
 
     public void Load(string name)
     {
@@ -17,4 +45,11 @@ public class MenuController : MonoBehaviour
         yield return new WaitForSeconds(.5f);
         SceneManager.LoadScene(name);
     }
+
+    private IEnumerator DisableCanvas(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        canvas.SetActive(false);
+    }
+
 }
